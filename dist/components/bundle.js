f8f35f0f653f619764a6f0f0ffc8e34ad9143062
enzoaicardi <enzo.aicardi@gmail.com>
2023-07-06 18:57:22 +0200

    updates

--- dist/components/bundle.js
+++ dist/components/bundle.js
@@ -1,3 +1,5 @@
 import "./heading.js"
 import "./nav.js"
 import "./sidebar.js"
+
+window['home_page'] = true;
--- uno.config.js
+++ uno.config.js
@@ -8,5 +8,22 @@ export default defineConfig({
             outFile: 'dist/assets/css/uno.css'
         }
     },
+    theme: {
+        colors: {
+            'primary': 'hsl(var(--color-primary))',
+            'primary-contrast': 'hsl(var(--color-primary-contrast))',
+            'text': 'hsl(var(--color-text))',
+            'text-contrast': 'hsl(var(--color-text-contrast))',
+            'text-highlight': 'hsl(var(--color-text-highlight))',
+            'backdrop': 'hsl(var(--color-backdrop))',
+            'background': 'hsl(var(--color-background))',
+            'background-contrast': 'hsl(var(--color-background-contrast))',
+            'gray': 'hsl(var(--color-gray))',
+            'gray-contrast': 'hsl(var(--color-gray-contrast))'
+        },
+    },
+    shortcuts: {
+        'w-sidebar': 'w-240'
+    },
     presets: [presetPruno()]
 })
